90b5311b6ba3cbad80b142b51cf503fef05bd8dd
Hsueh <40841121@gm.nfu.edu.tw>
2021-10-25 15:15:12 +0800

    add 增加計算bmi的程式碼

--- app/src/main/java/com/example/a20211025/MainActivity.java
+++ app/src/main/java/com/example/a20211025/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.a20211025;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText heightEditText = findViewById(R.id.height_content);
+        EditText weightEditText = findViewById(R.id.weight_content);
+
+
+        Button cal_btn = findViewById(R.id.cal_button);
+        cal_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                try {
+                    float height = Float.parseFloat(heightEditText.getText().toString())/100;
+                    float weight = Float.parseFloat(weightEditText.getText().toString());
+
+                    float result = weight / (height * height);
+
+                    Toast toast = Toast.makeText(
+                            MainActivity.this,
+                            Float.toString(result),
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+
+                } catch (
+                        NumberFormatException e
+                ) {
+                    //
+                }
+
+            }
+        });
     }
 }
